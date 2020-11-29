8feb7d16c14296c3f230bb1f47fcbcb6d3a4ee82
ahmetkaygisiz <ahmetkaygisiz17@gmail.com>
2020-11-29 21:28:32 +0300

    tutorial project

---
+++ backend/src/main/java/com/akua/backend/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.akua.backend.repository;
+
+import com.akua.backend.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
